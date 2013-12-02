9142739cb09391c0a2b795b28f56440304b214d6
Francois @fanf42 Armand <francois.armand@normation.com>
2013-12-02 19:13:35 +0100

    Found the bug

--- rudder-core/src/main/scala/com/normation/rudder/domain/policies/RuleDiff.scala
+++ rudder-core/src/main/scala/com/normation/rudder/domain/policies/RuleDiff.scala
@@ -35,6 +35,7 @@
 package com.normation.rudder.domain.policies
 
 import com.normation.utils.HashcodeCaching
+import com.normation.rudder.rule.category.RuleCategoryId
 
 
 /**
@@ -64,6 +65,7 @@ final case class ModifyRuleDiff(
   , modreasons          : Option[SimpleDiff[String]] = None
   , modIsActivatedStatus: Option[SimpleDiff[Boolean]] = None
   , modIsSystem         : Option[SimpleDiff[Boolean]] = None
+  , modCategory         : Option[SimpleDiff[RuleCategoryId]] = None
 ) extends RuleDiff with HashcodeCaching
 
 final case class ModifyToRuleDiff(
